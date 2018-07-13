b4f24a2a586e1d71a0a269a0897d023cbea7d6b4
Eliane Briand <eliane.c.briand@gmail.com>
2018-07-13 22:02:13 +0200

    sponsors

--- src/app/support-section/support-section.component.ts
+++ src/app/support-section/support-section.component.ts
@@ -1,13 +1,96 @@
 import { Component, OnInit } from '@angular/core';
+import {TeamMember} from '../team-roster/team-roster.component';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+
+
+export class Sponsor {
+  public name: string;
+  public image_src: string;
+  public sponsor_text: string;
+  public expanded;
+  public expanded_bool: boolean;
+
+  constructor(name: string, image_src: string, sponsor_text: string) {
+    this.name = name;
+    this.image_src = image_src;
+    this.sponsor_text = sponsor_text;
+    this.expanded_bool = false;
+    this.expanded = 'inactive' ;
+
+  }
+
+  toggleState() {
+    this.expanded_bool = !this.expanded_bool
+    this.expanded = this.expanded === 'active' ? 'inactive' : 'active';
+  }
+}
 
 @Component({
   selector: 'app-support-section',
   templateUrl: './support-section.component.html',
-  styleUrls: ['./support-section.component.css']
+  styleUrls: ['./support-section.component.css'],
+  animations: [
+    trigger('cardState', [
+      state('inactive', style({
+      })),
+      state('active',   style({
+      })),
+      transition('inactive => active', [
+        style({transform: 'translateY(100%)'}),
+        animate('200ms ease-in')
+      ]),
+      transition('active => inactive', [
+        style({transform: 'translateY(-20%)'}),
+        animate('200ms ease-out')
+      ])
+    ])
+  ]
 })
 export class SupportSectionComponent implements OnInit {
 
-  constructor() { }
+
+  sponsorsList = [
+    new Sponsor("FSDIE",
+      "http://2018.igem.org/wiki/images/7/72/T--GO_Paris-Saclay--FSDIE.png",
+      "The FSDIE is ..."),
+    new Sponsor("New England Biolab",
+      "http://2018.igem.org/wiki/images/f/f8/T--GO_Paris-Saclay--NEB.png",
+      "NEB is ..."),
+    new Sponsor("DesignBro",
+      "http://2018.igem.org/wiki/images/0/0f/T--GO_Paris-Saclay--DesignBro.png",
+      "DesignBro is ..."),
+    new Sponsor("MathWorks",
+      "http://2018.igem.org/wiki/images/a/aa/T--GO_Paris-Saclay--matworks.png",
+      "MathWorks is ..."),
+    new Sponsor("Integrated DNA Technologies",
+      "http://2018.igem.org/wiki/images/d/d4/T--GO_Paris-Saclay--IDT.png",
+      "IDT is ..."),
+    new Sponsor("Embassade de France aux USA",
+      "http://2018.igem.org/wiki/images/d/df/T--GO_Paris-Saclay--emba_fr_us.jpg",
+      "EmbaFrance is ...")
+  ]
+
+  constructor() {
+
+    this.shuffleArray(this.sponsorsList);
+  }
+
+  shuffleArray = function(array) {
+    var m = array.length, t, i;
+
+    // While there remain elements to shuffle
+    while (m) {
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+
+    return array;
+  }
 
   ngOnInit() {
   }
--- src/app/team-roster/team-roster.component.ts
+++ src/app/team-roster/team-roster.component.ts
@@ -1,21 +1,93 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 
+
+import {
+  Input
+} from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+
+export class TeamMember {
+  public name: string;
+  public image_src: string;
+  public bio: string;
+  public expanded;
+  public expanded_bool;
+  public expanded_bio: string;
+
+  constructor(name: string, image_src: string, bio: string, expanded_bio: string) {
+    this.name = name;
+    this.image_src = image_src;
+    this.bio = bio;
+    this.expanded = 'inactive';
+    this.expanded_bool = false;
+    this.expanded_bio = expanded_bio;
+  }
+
+  toggleState() {
+    this.expanded_bool = !this.expanded_bool;
+    this.expanded = this.expanded === 'active' ? 'inactive' : 'active';
+  }
+}
+
 @Component({
   selector: 'app-team-roster',
   templateUrl: './team-roster.component.html',
-  styleUrls: ['./team-roster.component.css']
+  styleUrls: ['./team-roster.component.css'],
+  animations: [
+    trigger('cardState', [
+      state('inactive', style({
+      })),
+      state('active',   style({
+      })),
+      transition('inactive => active', [
+        style({transform: 'translateY(100%)'}),
+        animate('200ms ease-in')
+      ]),
+      transition('active => inactive', [
+        style({transform: 'translateY(-20%)'}),
+        animate('200ms ease-out')
+      ])
+    ])
+  ]
 })
 export class TeamRosterComponent implements OnInit, AfterViewInit {
 
 
   membersList = [
-    {name: "Kenn P.", img_src: "assets/kenn.jpg", bio: "Very productive, innovating, he can take decisions and he did so many things for our team: Searching Sponsor, Biology, Collaboration ! And behind iGEM, he is a big fan of windsurfing, and loves the sea !" },
-    {name: "Guillaume G.", img_src: "assets/guillaume.jpg", bio: "Our second mathematician ! He knew nothing about biology before iGEM but now, he loves that ! He can spend hours to solve problems. And after iGEM ? Mountains and plays the violin. " },
-    {name: "Raphael G.", img_src: "assets/raphael.jpg", bio: "Both rest of know draw fond post as. It agreement defective to excellent. Feebly do engage of narrow. Extensive repulsive belonging depending if promotion be zealously as." },
-    {name: "JC Duroc", img_src: "assets/ceo.jpg", bio: "Abilities or he perfectly pretended so strangers be exquisite. Oh to another chamber pleased imagine do in. Went me rank at last loud shot an draw. Excellent so to no sincerity smallness." },
-    {name: "JC Duroc", img_src: "assets/ceo.jpg", bio: "Received shutters expenses ye he pleasant. Drift as blind above at up. No up simple county stairs do should praise as. Drawings sir gay together landlord had law smallest." },
-    {name: "JC Duroc", img_src: "assets/ceo.jpg", bio: "Him boisterous invitation dispatched had connection inhabiting projection. By mutual an mr danger garret edward an. Diverted as strictly exertion addition no disposal by stanhill." },
-    {name: "JC Duroc", img_src: "assets/ceo.jpg", bio: "On recommend tolerably my belonging or am. Mutual has cannot beauty indeed now sussex merely you. It possible no husbands jennings ye." }
+    new TeamMember("Kenn P.",
+      "assets/kenn.jpg",
+      "Very productive, innovating, he can take decisions and he did so many things for our team: Searching Sponsor, Biology, Collaboration ! And behind iGEM, he is a big fan of windsurfing, and loves the sea !",
+      "Hohoho"),
+    new TeamMember("Guillaume G.",
+      "assets/guillaume.jpg",
+      "Our second mathematician ! He knew nothing about biology before iGEM but now, he loves that ! He can spend hours to solve problems. And after iGEM ? Mountains and plays the violin. ",
+      ""),
+    new TeamMember("Raphael G.",
+      "assets/raphael.jpg",
+      "Both rest of know draw fond post as. It agreement defective to excellent. Feebly do engage of narrow. Extensive repulsive belonging depending if promotion be zealously as.",
+      ""),
+    new TeamMember("JC Duroc",
+      "assets/ceo.jpg",
+      "Abilities or he perfectly pretended so strangers be exquisite. Oh to another chamber pleased imagine do in. Went me rank at last loud shot an draw. Excellent so to no sincerity smallness." ,
+      ""),
+    new TeamMember("JC Duroc",
+      "assets/ceo.jpg",
+      "Abilities or he perfectly pretended so strangers be exquisite. Oh to another chamber pleased imagine do in. Went me rank at last loud shot an draw. Excellent so to no sincerity smallness." ,
+      ""),
+    new TeamMember("JC Duroc",
+      "assets/ceo.jpg",
+      "Abilities or he perfectly pretended so strangers be exquisite. Oh to another chamber pleased imagine do in. Went me rank at last loud shot an draw. Excellent so to no sincerity smallness." ,
+      ""),
+    new TeamMember("JC Duroc",
+      "assets/ceo.jpg",
+      "Abilities or he perfectly pretended so strangers be exquisite. Oh to another chamber pleased imagine do in. Went me rank at last loud shot an draw. Excellent so to no sincerity smallness." ,
+      "")
   ];
 
   constructor() {
