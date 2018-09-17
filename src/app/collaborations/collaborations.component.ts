4e18d1e0c090239fc4800e9c80a10328a3227582
Eliane Briand <eliane.c.briand@gmail.com>
2018-09-17 09:04:52 +0200

    intermediate state

--- src/app/collaborations/collaborations.component.ts
+++ src/app/collaborations/collaborations.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
+
+
 @Component({
   selector: 'app-collaborations',
   templateUrl: './collaborations.component.html',
@@ -7,7 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollaborationsComponent implements OnInit {
 
+
+
   constructor() { }
+
+
+  team_list: string[] = [
+    "Sorbonne U (France)",
+    "Bordeaux (France)",
+    "Toulouse (France)",
+    "Pasteur (France)",
+    "Copenhagen (Denmark)",
+    "Evry (France)",
+    "Fau Erlangen (Germany)",
+    "Tu Darmstadt (Germany)",
+    "Dusseldorf (Germany)",
+    "Eindhoven (Netherlands)",
+    "EFL (Switzerland)",
+    "Alto Helsinki (Finland)",
+    "Stuttgart (Germany)",
+    "U Born (Germany)",
+    "Leiden (Netherlands)",
+    "Aachen (Germany)",
+    "Wurzburg (Germany)",
+    "Bielefeld (Germany)",
+    "ETH Zurich (Switzerland)",
+    "Marburg (Germany)",
+    "Boku Viena (Austria) ",
+    "Paris Bettencourt (France)",
+    "Utrecht (Netherlands) ",
+    "Delaware (USA) ",
+    "Lippstad (Germany)",
+    "Tubigen (Germany)",
+    "Groningen (Netherlands)",
+    "New Castle (UK)",
+    "Taichung (Taiwan)",
+    "Hamburg (Germany)",
+    "Munich(Germany)"
+  ];
 
   ngOnInit() {
   }
